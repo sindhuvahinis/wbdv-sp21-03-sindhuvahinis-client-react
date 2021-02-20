edca273d3f2c7cc69569baf31d4169a1dfd29b4b
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-02-19 21:09:31 -0500

    creating course-table.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CourseManager from "./components/course-manager";
+import CourseEditor from "./components/course-editor";
 
 function App() {
   return (
     <div className='container-fluid'>
         <CourseManager/>
+        <CourseEditor/>
     </div>
   );
 }
---
+++ src/components/course-editor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseEditor = () => {
+    return (
+        <h1>Course Editor</h1>
+    )
+}
+
+export default CourseEditor
