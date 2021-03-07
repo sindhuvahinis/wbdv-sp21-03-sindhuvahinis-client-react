3633b246583a7dcc1531cc9edd42ca74342859ce
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-03-07 17:26:45 -0500

    Created module-service.js file

---
+++ src/services/module-service.js
@@ -0,0 +1,41 @@
+const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/sindhuvahinis/courses"
+const MODULES_URL = "https://wbdv-generic-server.herokuapp.com/api/sindhuvahinis/modules"
+
+export const findModulesForCourse = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`)
+        .then(response => response.json())
+
+export const createModuleForCourse = (courseId, module) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`, {
+        method: "POST",
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const deleteModule = (moduleId) =>
+    fetch(`${MODULES_URL}/${moduleId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+export const updateModule = (moduleId, module) =>
+    fetch(`${MODULES_URL}/${moduleId}`, {
+        method: "PUT",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(module)
+    })
+        .then(response => response.json())
+
+const api = {
+    findModulesForCourse,
+    createModuleForCourse,
+    deleteModule,
+    updateModule
+};
+
+export default api
