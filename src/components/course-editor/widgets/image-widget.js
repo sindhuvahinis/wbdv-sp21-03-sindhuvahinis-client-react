00fdbb9735bbc609f000be674ebe857d52a7d18e
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-02 22:44:58 +0530

    created image-widget.js file

---
+++ src/components/course-editor/widgets/image-widget.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const ImageWidget = ({widget, editing, setEditingWidget}) => {
+    return (
+        <div>
+            {
+                !editing &&
+                <img width={widget.width}
+                     height={widget.height}
+                     src={widget.src}/>
+            }
+            {
+                editing &&
+                <>
+                    Image URL
+                    <input className="form-control"
+                           value={widget.src}
+                           onChange={event => setEditingWidget({
+                               ...widget,
+                               src: event.target.value
+                           })}/>
+
+                    Image width
+                    <input className="form-control"
+                           value={widget.width}
+                           onChange={event => setEditingWidget({
+                               ...widget,
+                               width: event.target.value
+                           })}/>
+                    Image height
+                    <input className="form-control"
+                           value={widget.height}
+                           onChange={event => setEditingWidget({
+                               ...widget,
+                               height: event.target.value
+                           })}/>
+                </>
+            }
+        </div>
+    )
+}
+
+export default ImageWidget
