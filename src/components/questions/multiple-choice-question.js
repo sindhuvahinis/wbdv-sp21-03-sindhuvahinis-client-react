a59282cc28b4516bc5f9e4cad0fe9bfd26673bec
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-12 01:20:05 +0530

    created multiple-choice-question.js file

---
+++ src/components/questions/multiple-choice-question.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+
+const MultipleChoiceQuestion = ({question}) => {
+    const [yourAnswer, setYourAnswer] = useState("")
+    const [highLight, setHighLight] = useState(false)
+
+    return (
+        <div>
+            <h5>
+                {question.question}
+                {
+                    question.correct === yourAnswer &&
+                    <i className="fas fa-check ss-quiz-check ss-quiz-icon"/>
+                }
+                {
+                    question.correct !== yourAnswer &&
+                    <i className="fas fa-times ss-quiz-times ss-quiz-icon"/>
+                }
+
+            </h5>
+
+            <ul className="list-group">
+                {
+                    question.choices.map((choice) => {
+                        return (
+                            <li className={"list-group-item "
+                            + (highLight ? (choice === yourAnswer ? yourAnswer === question.correct ? " list-group-item-success" : "list-group-item-danger" : "") : "")
+                            + (highLight && choice === question.correct ? " list-group-item-success" : "")}>
+                                {
+                                    <label>
+                                        <input
+                                            onClick={() => {
+                                                setYourAnswer(choice)
+                                            }}
+                                            type="radio"
+                                            name={question._id}/> {choice}
+                                    </label>
+                                }
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <p>Your answer: {yourAnswer}</p>
+            {/*<p>{question.correct}</p>*/}
+            <button className="btn btn-success" onClick={() => setHighLight(true)}>
+                Grade
+            </button>
+        </div>
+    )
+}
+
+export default MultipleChoiceQuestion
