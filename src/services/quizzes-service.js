c0479087955018dfa0721e2ccad9ac0df2923aa1
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-19 21:07:49 +0530

    add submitQuiz to quizzes-service.js

--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -10,8 +10,20 @@ const findQuizById = (qid) => {
         .then(response => response.json())
 }
 
+const submitQuiz = (quizId, questions) => {
+    return fetch(`${QUIZZES_URL}/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(result => console.log(result))
+}
+
 export default {
     findAllQuizzes,
-    findQuizById
+    findQuizById,
+    submitQuiz
 }
 
